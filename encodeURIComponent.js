ba20f0e5f4bda46a8e7edc2b1aee6ed058adcd5a
Aminechakr <chakrelllah@gmail.com>
2023-08-03 10:50:42 +0200

    remove some stuff

--- encodeURIComponent.js
+++ encodeURIComponent.js
@@ -1,4 +1,4 @@
-const originalSlackWebhookUrl = 'https://hooks.slack.com/services/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';
+const originalSlackWebhookUrl = `https://${process.env.SLACK_WEBHOOK_URL}`;
 const encodedSlackWebhookUrl = encodeURIComponent(originalSlackWebhookUrl);
 console.log(encodedSlackWebhookUrl);
 
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 # test curl command
 curl --silent "https://api.github.com/repos/Aminechakr/test-tracker/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 # test encoding URI value 
-echo -n 'https://hooks.slack.com/services/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' | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
+echo -n 'https://hooks.slack.com/services/xxxxxxx/xxxxxxxx/xxxxxxxx' | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
