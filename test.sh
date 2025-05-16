77feb3e98e83f4e2fce1c17176f40759467f1999
aminechakr <chakrelllah@gmail.com>
2025-05-16 12:01:07 +0100

    Update: Load slack hook link as var

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
+# Load environment variables from .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 # test curl command
 curl --silent "https://api.github.com/repos/Aminechakr/test-tracker/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
-# test encoding URI value 
-echo -n 'https://hooks.slack.com/services/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' | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
+
+# test encoding URI value
+echo -n "$SLACK_WEBHOOK_URL" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
